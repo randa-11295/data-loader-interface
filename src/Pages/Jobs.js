9e784c037afc99a34b75ff04aa1e605cb1eeda2a
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-08-04 15:56:17 +0300

    make reusable table

--- src/Pages/Jobs.js
+++ src/Pages/Jobs.js
@@ -1,18 +1,20 @@
 import { Box } from "@mui/material";
+import ReusableTable from "../Components/Reusable/ReusableTable";
 
 const Jobs = () => {
-
+  const jobDataRes = [
+    { name: "job1", activation: "Active", lastRun: "1/12/2024" },
+    { name: "job2", activation: "not Active", lastRun: "1/12/2024" },
+    { name: "job3", activation: "pending ", lastRun: "1/12/2024" },
+    { name: "job4", activation: "Active", lastRun: "1/12/2024" },
+    { name: "job5", activation: "Active", lastRun: "1/12/2024" },
+  ];
 
   return (
- 
-      <Box sx={{ my: 5 }}>
-       jobs
-      </Box>
-    
+    <Box sx={{ my: 5 }}>
+      <ReusableTable data={jobDataRes} />
+    </Box>
   );
 };
 
 export default Jobs;
-
-
-
