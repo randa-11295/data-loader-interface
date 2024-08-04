4477f0c7d0bcc26fd7fd64d68b6b3dfc40ed8dcd
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-08-04 16:34:13 +0300

    add controlar btns at reusable tables

--- src/Components/Inputs/DesBtn.js
+++ src/Components/Inputs/DesBtn.js
@@ -1,42 +1,29 @@
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import { useSelector } from "react-redux";
 
-// this component used to make a description simple pop up on icon button 
-// its required to send icon component to render it inside icon btn 
-// and it required text description also to render it in side pop up 
+// this component used to make a description simple pop up on icon button
+// its required to send icon component to render it inside icon btn
+// and it required text description also to render it in side pop up
 
 export default function DesBtn(props) {
-  const language  = useSelector((state) => state.translation.language);
-
   return (
     <Tooltip
       title={props.text}
-      placement={  // control pop up place
-        props.place ||( !props.close
-          ? "top"
-          : language === "ar"
-          ? "left"
-          : "right")
+      placement={
+        // control pop up place
+        props.place || (!props.close ? "top" : "left")
       }
       arrow
     >
-      <IconButton 
-        color={!props.close ? "primary" : "default"} // make btn color gray if it close btn 
-        sx={ props.withoutStyle ? {}:(!props.close ? btnStyle(props.copy) : {color : props.primary ? "primary.main" : "default"}) } // removing all our custom style if it close btn 
-        onClick={props.fun} 
+      <IconButton
+        color={!props.close ? "primary" : "default"} // make btn color gray if it close btn
+  
+        onClick={props.fun}
         type={props.type || "button"}
-       
       >
         {/* te dynamic icon components send from parents */}
-        {props.children} 
+        {props.children}
       </IconButton>
     </Tooltip>
   );
 }
-
-const btnStyle = (copy)=>({
-  border: 1,
- borderRadius: "4px",
- padding: copy ? "5px" : { xs: "6px", sm: "8px", lg: "10px" },
-})
--- src/Components/Reusable/ReusableTable.js
+++ src/Components/Reusable/ReusableTable.js
@@ -7,20 +7,22 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import DesBtn from "../Inputs/DesBtn";
+import AnalyticsOutlinedIcon from "@mui/icons-material/AnalyticsOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
+    backgroundColor: theme.palette.secondary.main,
     color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
   },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
+    textAlight : "center"
   },
   // hide last border
   "&:last-child td, &:last-child th": {
@@ -28,37 +30,49 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-
-
 export default function ReusableTable({ data }) {
   const [tableTitlesHeaderArr, setTableTitlesHeaderArr] = useState([]);
 
   useEffect(() => {
     let titleTableData = [];
-    for (const property in data[1]) {
-      titleTableData.push(property);
+    if (data && data[1]) {
+      for (const property in data[1]) {
+        titleTableData.push(property);
+      }
     }
     setTableTitlesHeaderArr(titleTableData);
   }, [data]);
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+      <Table sx={{ minWidth: "min-content"  }} >
         <TableHead>
           <TableRow>
-            {tableTitlesHeaderArr.map((el) => (
-              <StyledTableCell key={el}>{el}</StyledTableCell>
+            {tableTitlesHeaderArr?.map((el) => (
+              <StyledTableCell align="center" key={el}>{el}</StyledTableCell>
             ))}
+            <StyledTableCell align="center">control</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data?.map((row) => (
             <StyledTableRow key={row.name}>
               {tableTitlesHeaderArr?.map((item) => (
-                <StyledTableCell component="th" scope="row">
+                <StyledTableCell align="center" component="td" scope="row">
                   {row[item]}
                 </StyledTableCell>
               ))}
+              <StyledTableCell align="center" component="td" scope="row">
+                <DesBtn>
+                  <AnalyticsOutlinedIcon />
+                </DesBtn>
+                <DesBtn>
+                  <EditOutlinedIcon />
+                </DesBtn>
+                <DesBtn>
+                  <DeleteOutlineOutlinedIcon />
+                </DesBtn>
+              </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
