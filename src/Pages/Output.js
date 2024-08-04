f50fe3c09e26255e553af3839e40a2179197f28d
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-08-04 19:30:37 +0300

    add tables data to sammary and output

--- src/Pages/Output.js
+++ src/Pages/Output.js
@@ -1,18 +1,24 @@
-import { Box } from "@mui/material";
+import ReusableTable from "../Components/Reusable/ReusableTable";
+import { useNavigate } from "react-router-dom";
 
 const Output = () => {
-
+  const navigate = useNavigate();
+  const jobDataRes = [
+    { name: "job1", activation: "Active", lastRun: "1/12/2024" },
+    { name: "job2", activation: "not Active", lastRun: "1/12/2024" },
+    { name: "job3", activation: "pending ", lastRun: "1/12/2024" },
+    { name: "job4", activation: "Active", lastRun: "1/12/2024" },
+    { name: "job5", activation: "Active", lastRun: "1/12/2024" },
+  ];
 
   return (
- 
-      <Box sx={{ my: 5 }}>
-       output
-      </Box>
-    
+    <>
+      <ReusableTable
+        data={jobDataRes}
+        editHandle={() => navigate("/edit-job")}
+      />
+    </>
   );
 };
 
 export default Output;
-
-
-
--- src/Pages/Summary.js
+++ src/Pages/Summary.js
@@ -1,15 +1,24 @@
-import { Box } from "@mui/material";
 import ReusableTable from "../Components/Reusable/ReusableTable";
+import { useNavigate } from "react-router-dom";
+
 const Summary = () => {
+  const navigate = useNavigate();
+  const jobDataRes = [
+    { name: "job1", activation: "Active", lastRun: "1/12/2024" },
+    { name: "job2", activation: "not Active", lastRun: "1/12/2024" },
+    { name: "job3", activation: "pending ", lastRun: "1/12/2024" },
+    { name: "job4", activation: "Active", lastRun: "1/12/2024" },
+    { name: "job5", activation: "Active", lastRun: "1/12/2024" },
+  ];
 
   return (
-      <Box sx={{ my: 5 }}>
-       <ReusableTable />
-      </Box>
+    <>
+      <ReusableTable
+        data={jobDataRes}
+        editHandle={() => navigate("/edit-job")}
+      />
+    </>
   );
 };
 
 export default Summary;
-
-
-
