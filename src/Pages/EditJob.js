6ae527f5da938ac7c925adbc2197d91015346f55
randa.ibrahim <randa.ibrahim@cartologic.com>
2024-08-08 02:10:17 +0300

    add new job

--- src/Pages/EditJob.js
+++ src/Pages/EditJob.js
@@ -12,11 +12,12 @@ const EditJob = (props) => {
     initialValues: { id: "", job_name: "", server_name: "", states: "" },
     // validationSchema: logInSchema,
     onSubmit: (values) => {
-      updateDataFetchHandle(values);
+      id ? updateDataFetchHandle(values) : addNewDataFetchHandle(values);
     },
   });
 
   useEffect(() => {
+    if (!id) return;
     axios
       .get("http://127.0.0.1:8090/api/collections/jobs/records/" + id)
       .then((res) => {
@@ -36,26 +37,52 @@ const EditJob = (props) => {
       .catch((err) => console.log(err));
   };
 
+  const addNewDataFetchHandle = (values) => {
+    axios
+      .post("http://127.0.0.1:8090/api/collections/jobs/records", values)
+
+      .then((res) => {
+        alert("done");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Card component="form" sx={{ p: 2 }} onSubmit={formik.handleSubmit}>
-      <InputTextCustom disabled={props.isShow} name="id" label="id" formik={formik} />
-      <InputTextCustom disabled={props.isShow} name="job_name" label="job name" formik={formik} />
-      <InputTextCustom disabled={props.isShow} name="server_name" label="server name" formik={formik} />
+ 
+      <InputTextCustom
+        disabled={props.isShow}
+        name="job_name"
+        label="job name"
+        formik={formik}
+      />
+      <InputTextCustom
+        disabled={props.isShow}
+        name="server_name"
+        label="server name"
+        formik={formik}
+      />
       {/* <InputTextCustom name="server_name" label="server_name" formik={formik} /> */}
 
-    {  !props.isShow &&  <Stack justifyContent="flex-end" direction="row">
-        <Button variant="outlined" onClick={formik.handleReset} sx={{ mx: 2 }}>
-          clear
-        </Button>
-        <Button
-          variant="contained"
-          sx={{ boxShadow: 0 }}
-          onClick={formik.handleSubmit}
-        >
-          Edit
-        </Button>
-        {/* <LoadBtn loading={loading} /> */}
-      </Stack>}
+      {!props.isShow && (
+        <Stack justifyContent="flex-end" direction="row">
+          <Button
+            variant="outlined"
+            onClick={formik.handleReset}
+            sx={{ mx: 2 }}
+          >
+            clear
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ boxShadow: 0 }}
+            onClick={formik.handleSubmit}
+          >
+            Edit
+          </Button>
+          {/* <LoadBtn loading={loading} /> */}
+        </Stack>
+      )}
     </Card>
   );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,6 @@ import HomePage from "../Pages/Home";
 import SummaryPage from "../Pages/Summary";
 import OutputPage from "../Pages/Output";
 import JobsPage from "../Pages/Jobs";
-import AddJobPage from "../Pages/AddJob";
 import EditJobPage from "../Pages/EditJob";
 import ConnectorSettingPage from "../Pages/ConnectorSetting";
 import GlobalSettingPage from "../Pages/GlobalSetting";
@@ -24,7 +23,7 @@ export const AppRouter = () => {
           <Route path="/" element={<SummaryPage />} />
           <Route path="/output" element={<OutputPage />} />
           <Route path="/jobs" element={<JobsPage />} />
-          <Route path="/add-job" element={<AddJobPage />} />
+          <Route path="/add-job" element={<EditJobPage />} />
           <Route path="/edit-job/:id" element={<EditJobPage />} />
           <Route path="/show-job/:id" element={<EditJobPage isShow />} />
           <Route path="/global-setting" element={<GlobalSettingPage />} />
